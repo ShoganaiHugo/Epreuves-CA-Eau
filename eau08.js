fa865d0e8a24ea2049b37264fa4825c43e34556f
Hugo Calimache <kalim1205@gmail.com>
2022-10-01 16:29:13 +0200

    Ajout 08

---
+++ eau08.js
@@ -0,0 +1,27 @@
+var numbers = '0123456789';
+
+function numOnly(str) {
+  // On gère les erreurs de paramètres
+  if (str.length === 0 || str.length > 1) {
+    return 'error';
+  } else {
+
+    // return !isNaN(str);
+
+    str = str.toString();
+    var count = 0;
+
+    while (count < str.length) {
+      if (numbers.includes(str.charAt(count))) {
+        count++;
+      } else {
+        return false;
+      }
+    }
+
+    return true;
+  }
+}
+
+
+console.log(numOnly(process.argv.slice(2)));
