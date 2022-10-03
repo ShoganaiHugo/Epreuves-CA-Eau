816fc901f6567f880611cdd0e1eb2f32bb008c59
Hugo Calimache <kalim1205@gmail.com>
2022-10-03 16:14:08 +0200

    Ajout 15

---
+++ eau15.js
@@ -0,0 +1,6 @@
+function felicitations() {
+  return "J’ai terminé l’Épreuve de l’Eau et c’était un peu plus difficile que la terre (mention spéciale aux 2 premiers exercices)."
+}
+
+
+console.log(felicitations());
