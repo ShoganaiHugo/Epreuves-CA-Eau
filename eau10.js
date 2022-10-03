7a42e03b445caf540f68fb99dc7f5eaded4e5b93
Hugo Calimache <kalim1205@gmail.com>
2022-10-03 11:53:27 +0200

    Ajout 10

--- eau10.js
+++ eau10.js
@@ -1,13 +1,39 @@
-function minToMax(str) {
+function indexWanted(arr) {
   // On gère les erreurs de paramètres
-  if (str.length !== 2 || !isNaN(str) === true) {
+  if (arr.length === 0) {
     return 'error';
 
   } else {
 
+    // On stocke le mot à chercher et le tableau qui acceuillera le reste du tableau initiale
+    const search = arr[arr.length - 1];
+    let newArray = [];
+
+    // On recréé un tableau sans le dernier mot
+    // Ici on pourrait utiliser arr = arr.splice(-1, 1) mais ça serait trop simple
+    for (let i = 0; i < arr.length - 1; i++) {
+      newArray[i] = arr[i];
+    }
+
+    // On va compter les itérations jusqu'à trouver le mot ou avoir parcouru toute la chaîne
+    let count = 0;
+    while (search !== newArray[count] && count <= newArray.length) {
+      count++;
+    }
+
+    // On compare le résultat avec la taille du nouveau tableau pour savoir si on s'est arrété avant la fin ou non
+    if (count === newArray.length + 1) {
+      if (search === newArray[count]) {
+        return count;
+      } else {
+        return -1;
+      }
+    } else {
+      return count;
+    }
 
   }
 }
 
 
-console.log(minToMax(process.argv.slice(2)));
+console.log(indexWanted(process.argv.slice(2)));
